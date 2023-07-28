ea7dad49fc35b7d4ea60020266929ec56d3d9ebf
Andriy <andriykrav@gmail.com>
2023-07-28 19:28:43 +0300

    app modifications and fixes following mentor's notices_3.0

--- city_api/crud.py
+++ city_api/crud.py
@@ -2,19 +2,21 @@ from typing import Optional, List
 from fastapi import HTTPException
 from sqlalchemy.orm import Session
 
+from city_api.models import City
 from city_api.schemas import CityCreate
-from city_api.models import City as CityModel, City
+from city_api.models import City as CityModel
 from temperature_api.models import Temperature as TemperatureModel
 
 
 def create_city(db: Session, city: CityCreate) -> City:
     db_city = CityModel(name=city.name, additional_info=city.additional_info)
     db.add(db_city)
+    commit_and_refresh(db, db_city)
     return db_city
 
 
-def get_cities(db: Session, skip: int = 0, limit: int = 100) -> List[City]:
-    cities = db.query(City).offset(skip).limit(limit).all()
+def get_cities(db: Session, skip: int = 0, limit: int = 10) -> List[City]:
+    return db.query(CityModel).offset(skip).limit(limit).all()
 
 
 def get_city(db: Session, city_id: int) -> Optional[City]:
@@ -27,6 +29,8 @@ def update_city(db: Session, city_id: int, city: CityCreate) -> City:
         raise HTTPException(status_code=404, detail="City not found")
     for table_field, city_name in city.model_dump().items():
         setattr(db_city, table_field, city_name)
+    commit_and_refresh(db, db_city)
+    return db_city
 
 
 def delete_city(db: Session, city_id: int) -> City:
@@ -34,6 +38,8 @@ def delete_city(db: Session, city_id: int) -> City:
     if not db_city:
         raise HTTPException(status_code=404, detail="City not found")
     db.delete(db_city)
+    commit_and_refresh(db, db_city)
+    return db_city
 
 
 def get_temperatures_by_city(
@@ -47,9 +53,6 @@ def get_temperatures_by_city(
     ).offset(skip).limit(limit).all()
 
 
-def commit_city(db: Session, city: CityCreate) -> City:
-    db_city = create_city(city)
-    db.add(db_city)
+def commit_and_refresh(db: Session, db_obj: object):
     db.commit()
-    db.refresh(db_city)
-    return db_city
+    db.refresh(db_obj)
--- city_api/router.py
+++ city_api/router.py
@@ -1,81 +1,44 @@
 from typing import List
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from sqlalchemy.exc import SQLAlchemyError
 
-from city_api.crud import create_city
-from dependencies import get_db
 from city_api.schemas import City, CityCreate
-from city_api.models import City as CityModel
-
+from city_api.crud import (
+    create_city, get_cities,
+    get_city, update_city, delete_city
+)
+from dependencies import get_db
 
 city_router = APIRouter()
 
 @city_router.post("/cities", response_model=City)
 def add_city(city: CityCreate, db: Session = Depends(get_db)):
-    try:
-        new_city = create_city(db, city)
-        db.commit()
-        db.refresh(new_city)
-        return new_city
-    except SQLAlchemyError as e:
-        db.rollback()
-        raise HTTPException(status_code=500, detail="Database Error")
+    return create_city(db, city)
 
 
 @city_router.get("/cities", response_model=List[City])
-def get_cities(
+def get_cities_list(
     skip: int = 0, limit: int = 10, db: Session = Depends(get_db)
 ):
-    cities = db.query(CityModel).offset(skip).limit(limit).all()
+    cities = get_cities(db, skip, limit)
     return cities
 
 
 @city_router.get("/cities/{city_id}", response_model=City)
-def get_city(city_id: int, db: Session = Depends(get_db)):
-    city = (
-        db.query(CityModel).filter(CityModel.id == city_id).first()
-    )
+def get_city_by_id(city_id: int, db: Session = Depends(get_db)):
+    city = get_city(db, city_id)
     if not city:
         raise HTTPException(status_code=404, detail="City not found")
     return city
 
 
 @city_router.put("/cities/{city_id}", response_model=City)
-def update_city(
+def update_city_by_id(
     city_id: int, city: CityCreate, db: Session = Depends(get_db)
 ):
-    try:
-        db_city = (
-            db.query(CityModel).filter(
-                CityModel.id == city_id
-            ).first()
-        )
-        if not db_city:
-            raise HTTPException(
-                status_code=404, detail="City not found"
-            )
-        for key, value in city.model_dump().items():
-            setattr(db_city, key, value)
-        db.commit()
-        db.refresh(db_city)
-        return db_city
-    except SQLAlchemyError as e:
-        db.rollback()
-        raise HTTPException(status_code=500, detail="Database Error")
+    return update_city(db, city_id, city)
 
 
 @city_router.delete("/cities/{city_id}")
-def delete_city(city_id: int, db: Session = Depends(get_db)):
-    try:
-        db_city = (
-            db.query(CityModel).filter(CityModel.id == city_id).first()
-        )
-        if not db_city:
-            raise HTTPException(status_code=404, detail="City not found")
-        db.delete(db_city)
-        db.commit()
-        return {"message": "City deleted successfully"}
-    except SQLAlchemyError as e:
-        db.rollback()
-        raise HTTPException(status_code=500, detail="Database Error")
+def delete_city_by_id(city_id: int, db: Session = Depends(get_db)):
+    return delete_city(db, city_id)
