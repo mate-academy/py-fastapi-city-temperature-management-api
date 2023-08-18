ec9dfa666dea5b5b8448be9411624276612a4e3d
neostyle88 <makssimm1@gmail.com>
2023-08-18 12:25:50 +0200

    implement temperature update & add .env

--- settings.py
+++ settings.py
@@ -1,10 +1,17 @@
+import os
+
 from pydantic.v1 import BaseSettings
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "City and temperature API"
 
-    DATABASE_URL: str | None = "sqlite+aiosqlite:///./city_catalog.db"
+    DATABASE_URL: str | None = os.getenv("DATABASE_URL")
+
+    WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
 
     class Config:
         case_sensitive = True
--- temperature/crud.py
+++ temperature/crud.py
@@ -1,9 +1,13 @@
-from fastapi import HTTPException, status
-from sqlalchemy import select, insert
+import asyncio
+from datetime import datetime
+
+import httpx
+from sqlalchemy import select
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from city import models as city_models
-from temperature import models, schemas
+from temperature import models
 
 
 async def get_all_temperatures(db: AsyncSession, city_id: int | None = None):
@@ -17,25 +21,55 @@ async def get_all_temperatures(db: AsyncSession, city_id: int | None = None):
     return [temperature[0] for temperature in temperature_list.fetchall()]
 
 
-async def create_temperature(
-    db: AsyncSession, temperature: schemas.TemperatureCreate
-):
-    city_query = select(city_models.DBCity).where(
-        city_models.DBCity.id == temperature.city_id
-    )
-    result = await db.execute(city_query)
-    existing_city = result.fetchone()
+async def get_weather(city_name: str, api_key: str) -> dict:
+    base_url = "https://api.weatherapi.com/v1/current.json"
+    params = {"q": city_name, "key": api_key}
 
-    if not existing_city:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="City not found"
+    async with httpx.AsyncClient() as client:
+        response = await client.get(base_url, params=params)
+        data = response.json()
+        return data
+
+
+async def update_temperature_for_city(city, client, db, api_key):
+    try:
+        weather_data = await get_weather(city_name=city.name, api_key=api_key)
+        current_temperature = weather_data["current"]["temp_c"]
+
+        temperature_instance = models.DBTemperature(
+            city_id=city.id,
+            date_time=datetime.now(),
+            temperature=current_temperature,
         )
-    query = insert(models.DBTemperature).values(
-        city_id=temperature.city_id,
-        date_time=temperature.date_time,
-        temperature=temperature.temperature,
-    )
-    result = await db.execute(query)
-    await db.commit()
-    resp = {**temperature.model_dump(), "id": result.lastrowid}
-    return resp
+
+        db.add(temperature_instance)
+        return f"Temperature for {city.name} updated"
+    except Exception as error:
+        return (
+            f"Error updating temperature for city {city.name}: "
+            f"city is not exist"
+        )
+
+
+async def update_temperature(db: AsyncSession, api_key: str):
+    try:
+        query = select(city_models.DBCity)
+        cities_list = await db.execute(query)
+        cities = cities_list.scalars()
+
+        async with httpx.AsyncClient() as client:
+            tasks = []
+            for city in cities:
+                tasks.append(
+                    asyncio.create_task(
+                        update_temperature_for_city(city, client, db, api_key)
+                    )
+                )
+            results = await asyncio.gather(*tasks)
+            await db.commit()
+
+        return results
+
+    except SQLAlchemyError as error:
+        await db.rollback()
+        print(f"Database error: {error}")
