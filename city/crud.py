4de2903778ea635e8231ecba624a8068da136f9d
oleksiikolii <igravkompik@gmail.com>
2023-08-28 16:54:21 +0300

    first part ready

---
+++ city/crud.py
@@ -0,0 +1,46 @@
+from sqlalchemy import select, insert
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from . import models, schemas
+
+
+async def get_all_cities(db: AsyncSession):
+    query = select(models.City)
+    cities_list = await db.execute(query)
+    return [city[0] for city in cities_list.fetchall()]
+
+
+async def create_city(db: AsyncSession, city: schemas.CityCreate):
+    query = insert(models.City).values(
+        name=city.name,
+        additional_info=city.additional_info
+    )
+    result = await db.execute(query)
+    await db.commit()
+
+    response = {**city.model_dump(), "id": result.lastrowid}
+    return response
+
+
+async def get_city_by_id(
+        db: AsyncSession,
+        city_id: int
+) -> schemas.City:
+    query = select(models.City).where(
+        models.City.id == city_id
+    )
+    city = await db.execute(query)
+    return city.scalar()
+
+
+async def delete_city(
+        db: AsyncSession,
+        city_id: int
+) -> schemas.City:
+    city = await get_city_by_id(db, city_id)
+
+    if city:
+        await db.delete(city)
+        await db.commit()
+
+    return city
---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+from city import router as city_router
+
+app = FastAPI()
+
+
+app.include_router(city_router.router)
+
+
+@app.get("/")
+def root() -> dict:
+    return {"message": "Hello World"}
