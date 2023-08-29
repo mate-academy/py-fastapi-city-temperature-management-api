e6624de1a6a9c0040f177217920175eeb7cb373c
Andriy Sydorenko <sidorenkoandrij217@gmail.com>
2023-08-30 00:13:00 +0300

    moved update temperatures method from utils.py to temperatures/crud.py

--- temperature/utils.py
+++ temperature/utils.py
@@ -1,13 +1,8 @@
 import os
 
 import httpx
-from sqlalchemy import select, insert
-from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import HTTPException
 
-from city import models as city_models
-from temperature import models as temperature_models
-from temperature import crud as temperature_crud
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -31,20 +26,3 @@ async def fetch_temperature_data(city_name: str):
     current_weather = response_data["current"]
     temp_c = current_weather["temp_c"]
     return temp_c
-
-
-async def update_temperatures(db: AsyncSession):
-    cities = await db.execute(select(city_models.DBCity))
-    for city in cities.scalars():
-        temp_c = await fetch_temperature_data(city_name=city.name)
-        temperature = await db.execute(select(temperature_models.DBTemperature).where(temperature_models.DBTemperature.city_id == city.id))
-        temperature = temperature.scalar()
-
-        if temperature:
-            temperature.temperature = temp_c
-        else:
-            await db.execute(insert(temperature_models.DBTemperature).values(
-                city_id=city.id,
-                temperature=temp_c
-            ))
-    await db.commit()
