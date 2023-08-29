d25b66b998a2ea4befc47e6e488d44831d73dc6e
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-08-29 13:26:26 +0300

    Create and fill in database.py

--- settings.py
+++ settings.py
@@ -5,6 +5,8 @@ class Settings(BaseSettings):
     PROJECT_NAME: str = "City temperature API"
 
     DATABASE_URL: str | None = "sqlite+aiosqlite:///./city.db"
+    # DATABASE_URL: str | None = "postgresql:" \
+    #                            "//user:password@postgresserver/db"
 
     class Config:
         case_sensitive = True
