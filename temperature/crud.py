56ceaaf0c63804d6f52cf313532cbe1488732c53
Andriy Sydorenko <sidorenkoandrij217@gmail.com>
2023-08-30 00:11:53 +0300

    fixed bug with city delete

--- temperature/crud.py
+++ temperature/crud.py
@@ -1,8 +1,11 @@
-from sqlalchemy import select
+from sqlalchemy import select, insert
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
 
 from temperature.models import DBTemperature
+from city import models as city_models
+from temperature import models as temperature_models
+from temperature.utils import fetch_temperature_data
 
 
 async def get_temperatures(db: AsyncSession, city_id: int = None):
@@ -16,8 +19,12 @@ async def get_temperatures(db: AsyncSession, city_id: int = None):
     return [temperature[0] for temperature in temperatures.fetchall()]
 
 
-async def update_temperatures_from_api(db: AsyncSession, city_id: int, temperature: float):
-    db_temperature = DBTemperature(city_id=city_id, temperature=temperature)
-    db.add(db_temperature)
+async def update_temperatures(db: AsyncSession):
+    cities = await db.execute(select(city_models.DBCity))
+    for city in cities.scalars():
+        temp_c = await fetch_temperature_data(city_name=city.name)
+        await db.execute(insert(temperature_models.DBTemperature).values(
+            city_id=city.id,
+            temperature=temp_c
+        ))
     await db.commit()
-    await db.refresh(db_temperature)
