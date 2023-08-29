4eba30355aa44c90d8bc345b88615237f06a427e
Andriy Sydorenko <sidorenkoandrij217@gmail.com>
2023-08-30 00:14:55 +0300

    removed unnecessary comment

--- settings.py
+++ settings.py
@@ -5,7 +5,6 @@ class Settings(BaseSettings):
     PROJECT_NAME: str = "City Temperature Management API"
 
     DATABASE_URL: str | None = "sqlite+aiosqlite:///./city_temperature_catalog.db"
-    # DATABASE_URL: str | None = "sqlite+aiosqlite:///./city_temperature_catalog.db"
 
     class Config:
         case_sensitive = True
