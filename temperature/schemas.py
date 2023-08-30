21ab67982bd99ddd7464e9f3a85becec3238ba67
Bohdan Yatsyna <bogdan.yatsyna@gmail.com>
2023-08-30 16:34:58 +0300

    Fill in schemas.py for temperature folder

---
+++ temperature/schemas.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+
+class TemperatureBase(BaseModel):
+    city_id: int
+    date_time: datetime
+    temperature: float
+
+
+class TemperatureCreate(TemperatureBase):
+    pass
+
+
+class Temperature(TemperatureBase):
+    id: int
