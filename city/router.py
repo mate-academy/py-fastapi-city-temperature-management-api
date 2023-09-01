1024d4ab3be104c5ea0509e1004b031676b423a4
Oleksandr Mamchur <irex1988@gmail.com>
2023-09-01 19:39:41 +0300

    added router and dependencies

---
+++ city/router.py
@@ -0,0 +1,48 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from city import schemas, crud
+from dependencies import get_db
+
+router = APIRouter()
+
+
+@router.get("/cities/", response_model=list[schemas.City])
+def list_authors(db: Session = Depends(get_db)):
+    return crud.get_all_cities(db=db)
+
+
+@router.post("/cities/", response_model=schemas.City)
+def create_city(city: schemas.CityCreate, db: Session = Depends(get_db)):
+    db_city = crud.get_city_by_name(db=db, name=city.name)
+    if db_city:
+        raise HTTPException(
+            status_code=400, detail="This city is already exist in DB"
+        )
+    return crud.create_city(db=db, city=city)
+
+
+@router.get("/cities/{city_id}", response_model=schemas.City)
+def retrieve_city_by_id(city_id: int, db: Session = Depends(get_db)):
+    db_city = crud.get_city_by_id(db=db, city_id=city_id)
+    if db_city:
+        return db_city
+    raise HTTPException(status_code=404, detail="City not found")
+
+
+@router.put("/cities/{city_id}", response_model=schemas.City)
+def update_city_by_id(
+    city_id: int,
+    updated_city: schemas.CityUpdate,
+    db: Session = Depends(get_db),
+):
+    db_city = crud.get_city_by_id(db=db, city_id=city_id)
+    if db_city:
+        return crud.update_city(
+            db=db, city_id=city_id, updated_city=updated_city
+        )
+
+
+@router.delete("/cities/{city_id}", response_model=dict)
+def delete_city_by_id(city_id: int, db: Session = Depends(get_db)):
+    return crud.delete_city(db=db, city_id=city_id)
--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI
 
+from city import router as city_router
+
 app = FastAPI()
+
+app.include_router(city_router.router)
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
