2f24e9c71366a37ffc1cc36e24c597a1bc2e0bef
yakhlamova <yanaakhlamova@gmail.com>
2023-09-04 13:28:54 +0300

    Added ReadMe

---
+++ main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+from city import router as city_router
+from temperature import router as temperature_router
+
+app = FastAPI()
+
+app.include_router(city_router.router)
+app.include_router(temperature_router.router)
