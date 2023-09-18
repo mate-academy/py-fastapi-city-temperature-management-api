ff3657c7f1af328eaddfec8629049c103de46670
Oleksandr Mamchur <irex1988@gmail.com>
2023-09-18 20:55:18 +0300

    Solution

--- alembic/env.py
+++ alembic/env.py
@@ -7,6 +7,7 @@ from alembic import context
 
 from city import models
 from temperature import models
+from database import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -21,7 +22,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = [models.Base.metadata]
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
--- city/models.py
+++ city/models.py
@@ -6,5 +6,5 @@ from database import Base
 class City(Base):
     __tablename__ = "city"
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(65), nullable=False, unique=True)
+    name = Column(String(255), nullable=False, unique=True)
     additional_info = Column(String(255), nullable=True)
