029e89fe73b730aa1dc5e56ebb91c8ae1db4a3b1
bythewaters <bythewaters800@gmail.com>
2023-09-25 01:22:58 +0200

    Initialize: new app city and added models and schemas for its used pydantic library

---
+++ city/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+
+from database import Base
+
+
+class City(Base):
+    __tablename__ = "city"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), nullable=False)
+    additional_info = Column(String(511))
+    
