ba87af7afff98810e1bf46ec5896ddb8bd360b5e
OlegatorLE <oleh.travin@gmail.com>
2023-11-05 16:04:14 +0200

    Added model City

---
+++ City/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String
+
+from database import Base
+
+
+class City(Base):
+    __tablename__ = "cities"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), index=True, nullable=False)
+    additional_info = Column(String(500))
