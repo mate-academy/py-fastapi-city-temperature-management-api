19c47ccfb57ae33c890587236a9eabf1441c1b52
eduardhabryd <eduardhabryd@gmail.com>
2023-11-06 17:38:27 +0200

    implement create_update_temperatures crud function

--- temperature/crud.py
+++ temperature/crud.py
@@ -1,10 +1,23 @@
+from datetime import datetime
 from sqlalchemy.orm import Session
 
-from temperature import models, schemas
+from temperature import models
+from city import models
+from scraper import get_city_temperature
 
 
 def create_update_temperatures(db: Session):
-    pass
+    all_cities = db.query(models.City).all()
+    for city in all_cities:
+        city_temperature = get_city_temperature(city.name)
+        db_temperature = models.Temperature(
+            city_id=city.id,
+            temperature=city_temperature,
+            date_time=datetime.now(),
+        )
+        db.add(db_temperature)
+        db.commit()
+        db.refresh(db_temperature)
 
 
 def get_temperatures(
