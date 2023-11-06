7408a0b6c73d776bd1be54a28f65369bf2e8a164
eduardhabryd <eduardhabryd@gmail.com>
2023-11-06 16:49:47 +0200

    implement Temperature model

---
+++ temperature/models.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+from sqlalchemy import Column, ForeignKey, Integer, DateTime, Float
+from sqlalchemy.orm import relationship
+
+from database import Base
+
+
+class Temperature(Base):
+    __tablename__ = "temperatures"
+
+    id = Column(Integer, primary_key=True, index=True)
+    date_time = Column(DateTime, index=True, default=datetime.utcnow)
+    temperature = Column(Float, index=True)
+    city_id = Column(Integer, ForeignKey("cities.id"))
