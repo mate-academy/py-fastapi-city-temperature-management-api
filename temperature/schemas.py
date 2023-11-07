70e47799c56404b3fd0db5071c44b17ceb9d427b
Illia <kononenko2109@gmail.com>
2023-11-07 15:48:11 +0200

    solution

---
+++ temperature/schemas.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+
+class TemperatureBase(BaseModel):
+    temperature: float
+
+
+class TemperatureCreate(TemperatureBase):
+    city_id: int
+
+
+class Temperature(TemperatureBase):
+    id: int
+    date_time: datetime
+    city_id: int
+
+    class Config:
+        orm_mode = True
