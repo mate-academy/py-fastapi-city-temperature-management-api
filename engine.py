fcd2c890701dd534aa1622a6c1658cb5eb8536cb
Taras Borshch <tarasborshch4@gmail.com>
2023-11-07 14:10:30 +0200

    Add settings file

--- engine.py
+++ engine.py
@@ -2,8 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+from settings import settings
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./city_temperature.db"
+
+SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
