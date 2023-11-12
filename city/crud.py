3668e4a739247d683589417c6400faee97426157
Petro Abramov <sebshe3@gmail.com>
2023-11-12 18:11:21 +0200

    add annotation

--- city/crud.py
+++ city/crud.py
@@ -1,17 +1,19 @@
+from typing import Any
+
 from sqlalchemy import select, insert
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from . import models, schemas
 
 
-async def get_all_cities(db: AsyncSession) -> list:
+async def get_all_cities(db: AsyncSession) -> list[models.City]:
     query = select(models.City)
     cities_list = await db.execute(query)
 
     return [city[0] for city in cities_list.fetchall()]
 
 
-async def create_city(db: AsyncSession, city: schemas.CityCreate):
+async def create_city(db: AsyncSession, city: schemas.CityCreate) -> dict[str, Any]:
     query = insert(models.City).values(
         name=city.name,
         additional_info=city.additional_info
