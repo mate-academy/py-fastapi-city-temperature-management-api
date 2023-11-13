19fd162eaa0f1e0e251f7ed2d575c0e42d5a0c0e
Kateryna Zadorozhna <katya.zadorozhna.00@gmail.com>
2023-11-13 19:31:41 +0200

    Made city/router.py async

--- city/router.py
+++ city/router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from dependencies import get_db
 from city import schemas, crud
@@ -8,23 +8,23 @@ router = APIRouter()
 
 
 @router.get("/cities/", response_model=list[schemas.CityList])
-def read_cities(db: Session = Depends(get_db)) -> list[schemas.CityList]:
-    return crud.get_all_cities(db=db)
+async def read_cities(db: AsyncSession = Depends(get_db)) -> list[schemas.CityList]:
+    return await crud.get_all_cities(db=db)
 
 
 @router.post("/cities/", response_model=schemas.CityList)
-def create_city(city: schemas.CityBase, db: Session = Depends(get_db)) -> schemas.CityList:
-    db_city = crud.get_city_by_name(db=db, city_name=city.name)
+async def create_city(city: schemas.CityBase, db: AsyncSession = Depends(get_db)) -> schemas.CityList:
+    db_city = await crud.get_city_by_name(db=db, city_name=city.name)
 
     if db_city:
         raise HTTPException(status_code=400, detail="This city already exists")
 
-    return crud.create_city(db=db, city=city)
+    return await crud.create_city(db=db, city=city)
 
 
 @router.get("/cities/{city_id}/", response_model=schemas.CityList)
-def read_single_city(city_id: int, db: Session = Depends(get_db)) -> schemas.CityList:
-    db_city = crud.get_city_by_id(db=db, city_id=city_id)
+async def read_single_city(city_id: int, db: AsyncSession = Depends(get_db)) -> schemas.CityList:
+    db_city = await crud.get_city_by_id(db=db, city_id=city_id)
 
     if db_city is None:
         raise HTTPException(status_code=400, detail="City not found")
@@ -33,10 +33,10 @@ def read_single_city(city_id: int, db: Session = Depends(get_db)) -> schemas.Cit
 
 
 @router.put("/cities/{city_id}/", response_model=schemas.CityList)
-def update_city(city_id: int, city: schemas.CityBase, db: Session = Depends(get_db)) -> schemas.CityList:
-    return crud.update_city(db=db, city_id=city_id, city=city)
+async def update_city(city_id: int, city: schemas.CityBase, db: AsyncSession = Depends(get_db)) -> schemas.CityList:
+    return await crud.update_city(db=db, city_id=city_id, city=city)
 
 
 @router.delete("/cities/{city_id}/")
-def delete_city(city_id: int, db: Session = Depends(get_db)) -> dict:
-    return crud.delete_city(db=db, city_id=city_id)
+async def delete_city(city_id: int, db: AsyncSession = Depends(get_db)) -> dict:
+    return await crud.delete_city(db=db, city_id=city_id)
