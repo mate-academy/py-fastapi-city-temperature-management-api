43395432b17bf1ea0a54c4566a4c5a7f56bbebbe
Kateryna Zadorozhna <katya.zadorozhna.00@gmail.com>
2023-11-13 19:44:24 +0200

    Made temperature/router.py async

--- temperature/router.py
+++ temperature/router.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 
-import requests
 from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy.orm import Session
+from httpx import AsyncClient
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from dependencies import get_db
 from settings import settings
@@ -17,13 +17,13 @@ parameters = {"key": settings.WEATHER_API_KEY}
 
 
 @router.get("/temperatures/", response_model=list[schemas.TemperatureList])
-def read_temperatures(db: Session = Depends(get_db)) -> list[schemas.TemperatureList]:
-    return crud.get_all_temperatures(db=db)
+async def read_temperatures(db: AsyncSession = Depends(get_db)) -> list[schemas.TemperatureList]:
+    return await crud.get_all_temperatures(db=db)
 
 
 @router.get("/temperatures/{city_id}/", response_model=schemas.TemperatureList)
-def read_temperature_by_city(city_id: int, db: Session = Depends(get_db)) -> schemas.TemperatureList:
-    db_temperature = crud.get_temperature_by_city_id(db=db, city_id=city_id)
+async def read_temperature_by_city(city_id: int, db: AsyncSession = Depends(get_db)) -> schemas.TemperatureList:
+    db_temperature = await crud.get_temperature_by_city_id(db=db, city_id=city_id)
 
     if db_temperature is None:
         raise HTTPException(status_code=400, detail="Temperature not found")
@@ -32,19 +32,21 @@ def read_temperature_by_city(city_id: int, db: Session = Depends(get_db)) -> sch
 
 
 @router.post("/temperatures/update/")
-async def create_temperatures(db: Session = Depends(get_db)) -> None:
-    cities = get_all_cities(db=db)
+async def create_temperatures(db: AsyncSession = Depends(get_db)) -> None:
+    cities = await get_all_cities(db=db)
     local_parameters = parameters
 
-    for city in cities:
-        local_parameters["q"] = city.name
-        response = requests.get(BASE_URL, params=local_parameters).json()
+    async with AsyncClient() as client:
+        for city in cities:
+            local_parameters["q"] = city.name
+            response = await client.get(BASE_URL, params=local_parameters)
+            response = response.json()
 
-        crud.create_temperature(
-            db=db,
-            city_id=city.id,
-            date_time=datetime.strptime(
-                response["current"]["last_updated"], "%Y-%m-%d %H:%M"
-            ),
-            temperature=response["current"]["temp_c"],
-        )
+            await crud.create_temperature(
+                db=db,
+                city_id=city.id,
+                date_time=datetime.strptime(
+                    response["current"]["last_updated"], "%Y-%m-%d %H:%M"
+                ),
+                temperature=response["current"]["temp_c"],
+            )
