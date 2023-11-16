89667daf35415ef9face6ae736f4f8762f9de6ea
Alex_Freud <NikiforenkoAS@i.ua>
2023-11-16 13:56:49 +0200

    Added City and Temperature models

---
+++ city/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy.orm import relationship
+
+from database import Base
+
+
+class City(Base):
+    __tablename__ = "city"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), nullable=False)
+    additional_info = Column(Text)
+
+    temperature = relationship("temperature.models.Temperature", back_populates="city")
