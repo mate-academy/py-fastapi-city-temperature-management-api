8a4b3cc835e3002bcd9df5632ab174880ab3aa84
Illia <kononenko2109@gmail.com>
2023-11-16 14:50:51 +0200

    fixed errors also used Black

--- temperature/crud.py
+++ temperature/crud.py
@@ -1,25 +1,23 @@
 from sqlalchemy import select, insert
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from temperature import models
+from temperature.models import Temperature
 
 
 async def list_temperatures(db: AsyncSession):
-    query = select(models.Temperature)
+    query = select(Temperature)
     temperature_list = await db.execute(query)
     return [temperature[0] for temperature in temperature_list.fetchall()]
 
 
 async def retrieve_temperature(db: AsyncSession, city_id: int):
-    query = select(models.Temperature).where(
-        models.Temperature.city_id == city_id
-    )
+    query = select(Temperature).where(Temperature.city_id == city_id)
     temperature = await db.execute(query)
     return [temperature[0] for temperature in temperature.fetchall()]
 
 
 async def create_temperature(db: AsyncSession, data: dict):
-    query = insert(models.Temperature).values(
+    query = insert(Temperature).values(
         temperature=data["temperature"],
         city_id=data["city_id"],
     )
