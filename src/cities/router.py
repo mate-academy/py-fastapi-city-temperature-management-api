7f548cabd47d197a45f5e8c8ef5b270473cb903f
andriyky <akrechovetskiy@gmail.com>
2023-12-16 19:09:54 +0200

    refactor: use `scalars` and `CRUDDetails`

--- src/cities/router.py
+++ src/cities/router.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.cities.crud import create_new_city, delete_city_by_id, get_all_cities
 from src.cities.models import DBCity
-from src.cities.schemas import City, CityCreate
+from src.cities.schemas import City, CityCreate, CRUDDetails
 from src.dependencies import get_db
 
 router = APIRouter(
@@ -18,21 +18,15 @@ async def read_cities(db: AsyncSession = Depends(get_db)) -> list[DBCity]:
     return await get_all_cities(db)
 
 
-@router.post(
-    "/",
-    response_description="Returns JSON with the ID of the created city.",
-)
+@router.post("/", response_model=CRUDDetails)
 async def create_city(
     city: CityCreate, db: AsyncSession = Depends(get_db)
-) -> dict[str, int]:
+) -> CRUDDetails:
     return await create_new_city(db, city)
 
 
-@router.delete(
-    "/{city_id}",
-    response_description="Returns JSON with the ID of the deleted city.",
-)
+@router.delete("/{city_id}", response_model=CRUDDetails)
 async def delete_city(
     city_id: int, db: AsyncSession = Depends(get_db)
-) -> dict[str, int]:
+) -> CRUDDetails:
     return await delete_city_by_id(db, city_id)
