6d76f2043ab52a0409310afffd20fa2fb99667cc
vshvanska <ladyvaleriya13@gmail.com>
2023-12-27 19:06:01 +0200

    Added settings in database.py

--- database.py
+++ database.py
@@ -2,7 +2,9 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./temerature.db"
+from settings import settings
+
+SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 
 engine = create_async_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
