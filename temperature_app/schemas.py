b13dd95707c7467f940ddf0014ffde4d2505bcff
Bohdan Marko <goldenbohdanm@gmail.com>
2024-02-09 17:13:44 +0200

    Implement schemas for Temperature model

---
+++ temperature_app/schemas.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+
+class Temperature(BaseModel):
+    id: int
+    city_id: int
+    date_time: datetime
+    temperature: float
+
+
+class TemperatureCreate(BaseModel):
+    city_id: int
+    date_time: datetime
+    temperature: float
