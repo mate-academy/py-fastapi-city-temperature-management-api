936d43505f616725bb58a5d20c3ef025cc87cfd2
Bohdan Marko <goldenbohdanm@gmail.com>
2024-02-09 14:04:37 +0200

    Implement City model in models.py

---
+++ city_app/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String
+
+from database import Base
+
+
+class City(Base):
+    __tablename__ = "city"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), unique=True, nullable=False)
+    additional_info = Column(String(511), nullable=True)
