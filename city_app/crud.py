1862ba51fb43163f3870cad19fcdac84d947434d
Bohdan Marko <goldenbohdanm@gmail.com>
2024-02-09 16:07:24 +0200

    Implement CRUD for City model

---
+++ city_app/crud.py
@@ -0,0 +1,51 @@
+from sqlalchemy import select, insert, delete, update
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from . import models, schemas
+
+
+async def get_all_cities(db: AsyncSession):
+    query = select(models.City)
+    city_list = await db.execute(query)
+    return [city[0] for city in city_list.fetchall()]
+
+
+async def create_city(db: AsyncSession, city: schemas.CityCreate):
+    query = insert(models.City).values(**city.dict())
+    result = await db.execute(query)
+    await db.commit()
+    response = {"id": result.lastrowid, **city.model_dump()}
+    return response
+
+
+async def get_city_by_id(db: AsyncSession, city_id: int):
+    query = select(models.City, models.City.id).where(models.City.id == city_id)
+    result = await db.execute(query)
+    city = result.scalars().first()
+    return city if city else None
+
+
+async def update_city(
+        db: AsyncSession,
+        city_id: int,
+        city_update: schemas.CityCreate
+):
+    city = await db.get(models.City, city_id)
+    if city is None:
+        return None
+
+    for field, value in city_update.dict().items():
+        setattr(city, field, value)
+
+    await db.commit()
+    await db.refresh(city)
+
+    return city
+
+
+async def delete_city(db: AsyncSession, city_id: int):
+    query = delete(models.City).where(models.City.id == city_id)
+    result = await db.execute(query)
+    await db.commit()
+    print(result)
+    return result
