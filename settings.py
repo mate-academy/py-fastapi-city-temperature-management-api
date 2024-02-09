4253c90be558cf8ff93d9d87b1e77971ff44f808
Bohdan Marko <goldenbohdanm@gmail.com>
2024-02-09 13:51:10 +0200

    Set up the database engine, configure settigs.py & dependencies.py

---
+++ settings.py
@@ -0,0 +1,14 @@
+from pydantic.v1 import BaseSettings
+
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "CityTemperature FastAPI Proj"
+
+    DATABASE_URL: str | None = "sqlite+aiosqlite:///./city_temp.db"
+
+    class Config:
+        case_sensitive = True
+        env_file = ".env"
+
+
+settings = Settings()
