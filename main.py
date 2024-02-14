66c6891b3824bee9a7419d37bef53d265cbff4cc
Bohdan Marko <goldenbohdanm@gmail.com>
2024-02-14 12:55:10 +0200

    Add a temperature router in main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 
 from city_app import router as city_router
+from temperature_app import router as temperature_router
 
 app = FastAPI()
 
 app.include_router(city_router.router)
+app.include_router(temperature_router.router)
 
 
 @app.get("/")
