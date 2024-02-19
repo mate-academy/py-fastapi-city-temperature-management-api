0f795420faa6fbfca6b69bd255101aef6e7f1616
Olexandr Hontarenko <sanya221138@gmail.com>
2024-02-19 16:49:13 +0200

    implement detail, create, delete for model city

--- city/router.py
+++ city/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from city import schemas, crud
@@ -10,3 +10,47 @@ router = APIRouter()
 @router.get("/cities/", response_model=list[schemas.CityList])
 async def read_cities(db: AsyncSession = Depends(get_db)):
     return await crud.get_all_city(db=db)
+
+
+@router.post("/cities/", response_model=schemas.CityList)
+async def create_city(
+        city: schemas.CityBase,
+        db: AsyncSession = Depends(get_db)
+):
+    return await crud.create_city(db=db, city=city)
+
+
+@router.get("/cities/{city_id}", response_model=schemas.CityList)
+async def detail_city(
+        city_id: int,
+        db: AsyncSession = Depends(get_db)
+):
+    city = await crud.get_city(db=db, city_id=city_id)
+    if city is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return city
+
+
+@router.put("/cities/{city_id}", response_model=schemas.CityList)
+async def update_city(
+        city_id: int,
+        city: schemas.CityBase,
+        db: AsyncSession = Depends(get_db)
+
+):
+    city = await crud.update_city(db=db, city_id=city_id, city=city)
+    if city is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return city
+
+
+@router.delete("/cities/{city_id}", response_model=schemas.CityList)
+async def delete_city(
+        city_id: int,
+        db: AsyncSession = Depends(get_db)
+
+):
+    city = await crud.delete_city(db=db, city_id=city_id)
+    if city is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return city
