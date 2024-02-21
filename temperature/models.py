5e7740d2e66eb6d98ab957f48ebd3fb44a4a2be8
Olexandr Hontarenko <sanya221138@gmail.com>
2024-02-21 14:51:04 +0200

    add relationship in City, Temperature date_time setting current time, and create migrations

--- temperature/models.py
+++ temperature/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, ForeignKey, DateTime
+from sqlalchemy import Column, Integer, ForeignKey, DateTime, func, Float
 from sqlalchemy.orm import relationship
 
 from city.models import City
@@ -7,8 +7,10 @@ from database import Base
 
 class Temperature(Base):
     __tablename__ = "temperatures"
+
     id = Column(Integer, primary_key=True, index=True)
     city_id = Column(Integer, ForeignKey("cities.id"))
+    date_time = Column(DateTime(timezone=True), default=func.now())
+    temperature = Column(Float)
+
     city = relationship(City)
-    date_time = Column(DateTime)
-    temperature = Column(Integer)
