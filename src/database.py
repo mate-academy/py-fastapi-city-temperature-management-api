bc45c4f3d9dda2018dbc4fa212b2e5ed5b8d6972
Bohdan Kolomiiets <kb907406@gmail.com>
2024-02-27 14:52:50 +0200

    Delete not needed imports and get_db function

--- src/database.py
+++ src/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./city_temperature_management.db"
 
@@ -10,12 +10,3 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
-
-def get_db() -> Session:
-    db = SessionLocal()
-
-    try:
-        yield db
-    finally:
-        db.close()
