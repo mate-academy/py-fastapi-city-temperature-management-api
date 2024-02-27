9c081d5b19445f69dfa6c045f89b75962f848f03
alexh <olexandrgauptf@gmail.com>
2024-02-27 23:50:02 +0200

    Solutions

---
+++ dependenci.py
@@ -0,0 +1,22 @@
+from typing import Annotated
+
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from database import SessionLocal
+
+
+async def get_db() -> AsyncSession:
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        await db.close()
+
+
+async def common_parameters(q: str | None = None, skip: int = 0, limit: int = 100):
+    return {"q": q, "skip": skip, "limit": limit}
+
+
+CommonsDep = Annotated[dict, Depends(common_parameters)]
