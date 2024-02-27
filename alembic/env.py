3ad1b8e1f3a43ba201d4312c6f3584cd590656e5
Bohdan Kolomiiets <kb907406@gmail.com>
2024-02-27 14:32:12 +0200

    Fix alembic env file

--- alembic/env.py
+++ alembic/env.py
@@ -5,7 +5,9 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from src.city.models import Base as CityBase
+from src.database import Base
+from src.city.models import City
+from src.temperature.models import CityTemperature
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -20,7 +22,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = [CityBase.metadata]
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
