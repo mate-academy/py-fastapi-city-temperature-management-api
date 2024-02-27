06c1dceb7b7a8aebfd95e728858910c96cee2393
Bohdan Kolomiiets <kb907406@gmail.com>
2024-02-27 12:59:19 +0200

    Implement the main file in application that ties every app together.

---
+++ src/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+from src.city.router import router as city_router
+
+app = FastAPI()
+
+app.include_router(city_router)
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello Bigger App"}
+
