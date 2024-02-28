67d5cdb6217374543fe1737551936d38554646fa
Bohdan Kolomiiets <kb907406@gmail.com>
2024-02-28 17:22:40 +0200

    Include router from temperature model to main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 
 from src.city.router import router as city_router
+from src.temperature.router import router as temperature_router
 
 app = FastAPI()
 
 app.include_router(city_router)
+app.include_router(temperature_router)
 
 
 @app.get("/")
