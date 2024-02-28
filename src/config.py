84b9b3a0037cf93a64cf385b767a0679b4bcb42a
Bohdan Kolomiiets <kb907406@gmail.com>
2024-02-28 17:20:15 +0200

    Add weather variables

--- src/config.py
+++ src/config.py
@@ -1,3 +1,5 @@
+import os
+
 from pydantic.v1 import BaseSettings
 
 
@@ -5,6 +7,10 @@ class Settings(BaseSettings):
     PROJECT_NAME: str = "FastAPI city temperature management API"
 
     DATABASE_URL: str | None = "sqlite+aiosqlite:///./city_temperature_management.db"
+
+    WEATHER_API_KEY: str | None = os.getenv("WEATHER_API_KEY")
+
+    CURRENT_WEATHER_URL: str | None = "http://api.weatherapi.com/v1/current.json"
 
     class Config:
         case_sensitive = True
