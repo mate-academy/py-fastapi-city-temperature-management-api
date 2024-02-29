28d49c99bcc4a949933b7de0f819e6f2e3638b30
Maxim V <m4k51m15k4m@gmail.com>
2024-02-29 14:08:11 +0200

    complete city/router.py

--- city/crud.py
+++ city/crud.py
@@ -3,14 +3,17 @@ from sqlalchemy.orm import Session
 from . import models, schemas
 
 
-def get_all_cities(db: Session):
+def get_all_cities(db: Session,
+                   ) -> list[models.DBCity]:
     return (db
             .query(models.DBCity)
             .all()
             )
 
 
-def get_city_by_name(db: Session, name: str):
+def get_city_by_name(db: Session,
+                     name: str,
+                     ) -> models.DBCity | None:
     return (db
             .query(models.DBCity)
             .filter(models.DBCity.name == name)
@@ -18,7 +21,9 @@ def get_city_by_name(db: Session, name: str):
             )
 
 
-def get_city_by_id(db: Session, city_id: int):
+def get_city_by_id(db: Session,
+                   city_id: int,
+                   ) -> models.DBCity | None:
     return (db
             .query(models.DBCity)
             .filter(models.DBCity.id == city_id)
@@ -26,7 +31,9 @@ def get_city_by_id(db: Session, city_id: int):
             )
 
 
-def create_city(db: Session, city: schemas.CityCreate):
+def create_city(db: Session,
+                city: schemas.CityCreate,
+                ) -> models.DBCity:
     db_city = models.DBCity.model_dump(city)
     db.add(db_city)
     db.commit()
@@ -35,7 +42,23 @@ def create_city(db: Session, city: schemas.CityCreate):
     return db_city
 
 
-def delete_city(db: Session, city_id: int):
+def update_city(db: Session,
+                city_id: int,
+                city_data: schemas.CityBase,
+                ) -> models.DBCity | None:
+    city = get_city_by_id(db=db, city_id=city_id)
+    if not city:
+        return None
+    for attr, value in city_data.model_dump().items():
+        setattr(city, attr, value)
+    db.commit()
+    db.refresh(city)
+    return city
+
+
+def delete_city(db: Session,
+                city_id: int,
+                ) -> None:
     (db
      .query(models.DBCity)
      .filter(models.DBCity.id == city_id)
--- city/router.py
+++ city/router.py
@@ -1,8 +1,52 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
+from city import crud, schemas
 from dependencies import get_db
-from . import schemas, crud
-
 
 router = APIRouter()
+
+
+@router.get("/cities/", response_model=list[schemas.City])
+def read_cities(db: Session = Depends(get_db),
+                ) -> list[schemas.City]:
+    return crud.get_all_cities(db=db)
+
+
+@router.post("/cities/", response_model=schemas.City)
+def create_city(city: schemas.CityCreate,
+                db: Session = Depends(get_db),
+                ) -> schemas.City:
+    return crud.create_city(db=db, city=city)
+
+
+@router.delete("/cities/{city_id}/")
+def delete_city(city_id: int,
+                db: Session = Depends(get_db),
+                ) -> Exception | dict[str, schemas]:
+    city = crud.get_city_by_id(db=db, city_id=city_id)
+    if city:
+        crud.delete_city(db=db, city_id=city_id)
+        return {"message": "City deleted"}
+    raise HTTPException(status_code=404, detail="City not found")
+
+
+@router.get("/cities/{city_id}/", response_model=schemas.City)
+def read_city(city_id: int,
+              db: Session = Depends(get_db),
+              ) -> schemas | Exception:
+    city = crud.get_city_by_id(db=db, city_id=city_id)
+    if city:
+        return city
+    raise HTTPException(status_code=404, detail="City not found")
+
+
+@router.put("/cities/{city_id}/", response_model=schemas.City)
+def update_city(city_id: int,
+                city: schemas.CityBase,
+                db: Session = Depends(get_db),
+                ) -> schemas.City | Exception:
+    updated_city = crud.update_city(db=db, city_id=city_id, city_data=city)
+    if updated_city:
+        return updated_city
+    raise HTTPException(status_code=404, detail="City not found")
