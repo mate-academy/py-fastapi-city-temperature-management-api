e08325e42cdfcab2e26cc54362117f81576ed003
Maxim V <m4k51m15k4m@gmail.com>
2024-02-29 11:12:38 +0200

    add database.py , update requirements.txt

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ from pydantic.v1 import BaseSettings
 class Settings(BaseSettings):
     PROJECT_NAME: str = "FastAPI City Temperature"
 
-    DATABASE_URL: str = "sqlite:///./city_temperature.db"
+    DATABASE_URL: str | None = "sqlite+aiosqlite:///./city_temperature.db"
 
     class Config:
         case_sensitive = True
