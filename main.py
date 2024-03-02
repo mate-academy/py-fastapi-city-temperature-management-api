76ddedf13be3d88e00c0861e8268d44e70052894
Oleksii Yaroshenko <a.s.yaroshenko.zp@gmail.com>
2024-03-02 21:29:49 +0200

    title and summary for task

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from city import router as city_router
 from temperature import router as temperature_router
 
 
-app = FastAPI()
+app = FastAPI(title="Module Python Core/FastAPI in Details",
+              description="Task 'py-fastapi-city-temperature-management-api'",
+              summary="FastAPI application that manages city data and their corresponding temperature data")
 app.include_router(city_router.router)
 app.include_router(temperature_router.router)
 
