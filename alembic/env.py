0ebf01bcdf1a739431451cc914b158d98b04e105
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2024-03-13 20:48:24 +0200

    Comment line of code for future

--- alembic/env.py
+++ alembic/env.py
@@ -5,7 +5,8 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from city.models import Base
+from city import models as city_models
+from temperature import models as tem_models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -20,7 +21,10 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = [Base.metadata]
+target_metadata = [
+    city_models.Base.metadata,
+    # tem_models.Base.metadata
+]
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
