2e23c1d8da88fee77bd109492476036300175186
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2024-03-13 20:53:32 +0200

    Create Pydantic models for Temperature table

---
+++ temperature/schemas.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+
+class TemperatureBase(BaseModel):
+    city_id: int
+    date_time: datetime
+    temp_c: float
+    temp_f: float
+
+
+class CreateTemperature(TemperatureBase):
+    pass
+
+
+class UpdateTemperature(TemperatureBase):
+    id: int
+
+
+class Temperature(TemperatureBase):
+    id: int
