43ebaa9dbb1a5a2ff607510c371febac697083cb
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2024-03-19 23:10:26 +0200

    Remove update_db_object function to helper

--- dependencies.py
+++ dependencies.py
@@ -1,5 +1,4 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-from pydantic import BaseModel
 
 from database import SessionLocal
 
@@ -7,17 +6,3 @@ from database import SessionLocal
 async def get_db() -> AsyncSession:
     async with SessionLocal() as db:
         yield db
-
-
-async def update_db_object(
-        db: AsyncSession,
-        current_object: BaseModel,
-        new_object: BaseModel,
-):
-    """ Update fields of any SQLAlchemy object """
-    for field, value in new_object.dict(exclude_unset=True).items():
-        setattr(current_object, field, value)
-
-    await db.refresh(current_object)
-
-    return current_object
