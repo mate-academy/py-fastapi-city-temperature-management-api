9a9ae8e570d02b80c3dd8d41d5ab2ce146a17441
Marian Kostiuk <131563162+kostiukmkalne@users.noreply.github.com>
2024-03-25 22:56:58 -0500

    solution

---
+++ app/api/schemas.py
@@ -0,0 +1,40 @@
+from pydantic import BaseModel
+from datetime import datetime
+from typing import List, Optional
+
+
+class CityBase(BaseModel):
+    name: str
+    additional_info: Optional[str] = None
+
+
+class CityCreate(CityBase):
+    pass
+
+
+class City(CityBase):
+    id: int  # noqa:VNE003
+    temperatures: List['Temperature'] = []
+
+    class Config:
+        from_attributes = True
+
+
+class TemperatureBase(BaseModel):
+    date_time: datetime
+    temperature: float
+
+
+class TemperatureCreate(TemperatureBase):
+    pass
+
+
+class Temperature(TemperatureBase):
+    id: int
+    city_id: int
+
+    class Config:
+        from_attributes = True
+
+
+City.update_forward_refs()
